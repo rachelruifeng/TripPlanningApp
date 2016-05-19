3b8fe8d192e2c5c0315abc5622fe66c56d0666fc
Rachel Feng <rachel.feng@schneider-electric.com>
2016-05-19 21:09:10 +0930

    Added bower dependency for JQuery and used JQuery.

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,12 +1,29 @@
 (function() {
-    var username = document.getElementById("username");
-    username.innerHTML = "Jie Dong";
+//    var username = $("#username");
+//    username.text("Jie Dong");
+//
+//    var main = $("#main");
+//    main.on("onmouseenter", function() {
+//        main.style["backgound-color"] = "#888";
+//    });
+//    main.on("mouseleave", function() {
+//        main.style["backgound-color"] = "";
+//    });
+//
+//    var menuItems = $("ul.menu li a");
+//    menuItems.on("click", function () {
+//        var me = $(this);
+//        alert(me.text());
+    //    });
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function() {
-        main.style["backgound-color"] = "#888";
-    };
-    main.onmouseleave = function() {
-        main.style["backgound-color"] = "";
-    };
-})()
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function() {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show");
+        } else {
+            $(this).text("Hide");
+        }
+    }); 
+})();
